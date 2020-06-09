40c7d538f5a4d190a3822c42ee4e334c88eefbcb
RyanVillenaUCR <rvill028@ucr.edu>
2020-06-08 22:21:31 -0700

    Add Snippet field

--- src/main/java/voyager/Result.java
+++ src/main/java/voyager/Result.java
@@ -3,8 +3,10 @@ package voyager;
 public class Result {
     public String url;
     public String title;
-    public Result(String URL, String title){
+    public String snippet;
+    public Result(String URL, String title, String snippet){
         this.url = URL;
         this.title = title;
+        this.snippet = snippet;
     }
 }
